c8344a110688e03986967d8fe9440745f919e67c
almadediamantee <acmoyanofernandez@escuelasproa.edu.ar>
2024-10-29 10:21:12 -0300

    boton de desconexion

--- desafio.ts
+++ desafio.ts
@@ -2,16 +2,24 @@
 document.getElementById('connect-btn')!.addEventListener('click', function() {
     const connectionStatus = document.getElementById('connection-status') as HTMLElement;
     const portsSelect = document.getElementById('ports') as HTMLSelectElement;
+{}
+document.getElementById('disconnect-btn')!.addEventListener('click', function() {
+        const connectionStatus = document.getElementById('connection-status') as HTMLElement;
+        const disconnectButton = document.getElementById('disconnect-btn') as HTMLButtonElement;
+        const connectButton = document.getElementById('connect-btn') as HTMLButtonElement;
 
     // Verificar si se ha seleccionado un puerto válido.
     if (portsSelect.value !== "" && portsSelect.value !== "Elige un puerto") {
         connectionStatus.textContent = "Estado: Conectado";
         connectionStatus.style.color = "green";
         alert(`Puerto ${portsSelect.value} seleccionado y conectado.`);
+
+
     } else {
         connectionStatus.textContent = "Estado: Desconectado";
         connectionStatus.style.color = "red";
         alert("Por favor, selecciona un puerto válido.");
+        
     }
 });
 
