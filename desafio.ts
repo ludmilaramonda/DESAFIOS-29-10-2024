3aeb2775be5a59067045dbadc6bde8ff26934fb3
ludmilaramonda <145378638+ludmilaramonda@users.noreply.github.com>
2024-11-19 09:08:13 -0300

    lud

--- desafio.ts
+++ desafio.ts
@@ -2,14 +2,7 @@
 document.getElementById('connect-btn')!.addEventListener('click', function() {
     const connectionStatus = document.getElementById('connection-status') as HTMLElement;
     const portsSelect = document.getElementById('ports') as HTMLSelectElement;
-});
-document.getElementById('disconnect-btn')!.addEventListener('click', function() {
-        const connectionStatus = document.getElementById('connection-status') as HTMLElement;
-        const disconnectButton = document.getElementById('disconnect-btn') as HTMLButtonElement;
-        const connectButton = document.getElementById('connect-btn') as HTMLButtonElement;
-
-    // Verificar si se ha seleccionado un puerto válido.
-        const portsSelect = document.getElementById('ports') as HTMLSelectElement;
+    
     if (portsSelect.value !== "" && portsSelect.value !== "Elige un puerto") {
         connectionStatus.textContent = "Estado: Conectado";
         connectionStatus.style.color = "green";
@@ -22,20 +15,30 @@ document.getElementById('disconnect-btn')!.addEventListener('click', function() 
         alert("Por favor, selecciona un puerto válido.");
         
     }
+
+
+});
+document.getElementById('desconnect-btn')!.addEventListener('click', function() {
+        const connectionStatus = document.getElementById('connection-status') as HTMLElement;
+        const disconnectButton = document.getElementById('desconnect-btn') as HTMLButtonElement;
+        const connectButton = document.getElementById('connect-btn') as HTMLButtonElement;
+
+    
+        document.getElementById('desconnect-btn')!.addEventListener('click', () => {
+    
+            console.log("Puerto desconectado.");
+        
+            // Reiniciar la página
+            location.reload();
+        });
 });
 
 // Agrega un listener al elemento select 'ports' para manejar la selección de un puerto.
 document.getElementById('ports')!.addEventListener('change', function() {
     const selectedPort = (this as HTMLSelectElement).value;
     if (selectedPort !== "Elige un puerto") {
-        alert(`Puerto ${selectedPort} seleccionado.`);
+        alert(`Puerto ${selectedPort} seleccionado.`); 
     }
 });
 
-document.getElementById('desconnect-btn')!.addEventListener('click', () => {
-    // Desconectar puerto (si fuera necesario, agrega lógica específica aquí)
-    console.log("Puerto desconectado.");
 
-    // Reiniciar la página
-    location.reload();
-});
